9dc05ce692abe5f4967aec4fc3faa3e92300249a
sunmi <ysm_0405@naver.com>
2025-03-16 18:29:33 +0900

    리포지토리 개발

--- src/main/kotlin/com/sunmi/portfolio/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/sunmi/portfolio/presentation/repository/PresentationRepository.kt
@@ -1,5 +1,41 @@
 package com.sunmi.portfolio.presentation.repository
 
-class PresentationRepository {
+import com.sunmi.portfolio.domain.entity.*
+import com.sunmi.portfolio.domain.repository.*
+import org.springframework.stereotype.Repository
+
+@Repository
+class PresentationRepository(
+    private val achievementRepository: AchievementRepository,
+    private val experienceRepository: ExperienceRepository,
+    private val introductionRepository: IntroductionRepository,
+    private val linkRepository: LinkRepository,
+    private val projectRepository: ProjectRepository,
+    private val skillRepository: SkillRepository
+) {
+
+    fun getActiveAchievements(): List<Achievement> {
+        return achievementRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveExperiences(): List<Experience> {
+        return experienceRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveIntroductions(): List<Introduction> {
+        return introductionRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveLinks(): List<Link> {
+        return linkRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveProjects(): List<Project> {
+        return projectRepository.findAllByIsActive(true)
+    }
+
+    fun getActiveSkills(): List<Skill> {
+        return skillRepository.findAllByIsActive(true)
+    }
 
 }
