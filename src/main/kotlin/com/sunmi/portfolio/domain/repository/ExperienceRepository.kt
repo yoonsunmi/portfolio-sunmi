f0536ba008c02de9b1fedb004b41d69004b0023a
sunmi <ysm_0405@naver.com>
2025-03-16 15:50:46 +0900

    리포지토리 개발 수정

--- src/main/kotlin/com/sunmi/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/sunmi/portfolio/domain/repository/ExperienceRepository.kt
@@ -6,6 +6,7 @@ import java.util.*
 
 interface ExperienceRepository:JpaRepository<Experience, Long> {
 
+    // 일대 다 관계라서 이대로 쓰면 성능에 좋지 않음 !
     fun findAllByIsActive(isActive: Boolean): List<Experience>
 
     override fun findById(id: Long): Optional<Experience>
--- src/main/kotlin/com/sunmi/portfolio/domain/repository/HttpInterfaceRepository.kt
+++ src/main/kotlin/com/sunmi/portfolio/domain/repository/HttpInterfaceRepository.kt
@@ -2,7 +2,10 @@ package com.sunmi.portfolio.domain.repository
 
 import com.sunmi.portfolio.domain.entity.HttpInterface
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDateTime
 
 interface HttpInterfaceRepository:JpaRepository<HttpInterface, Long> {
+
+    fun countAllByCreatedDateTimeBetween(start: LocalDateTime, end: LocalDateTime): Long
 
 }
