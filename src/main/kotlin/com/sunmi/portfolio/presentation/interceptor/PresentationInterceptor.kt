5a9d9b8bbd32d5944b815d99fd682b090173a4aa
sunmi <ysm_0405@naver.com>
2025-03-18 00:05:30 +0900

    인터셉터 개발

--- src/main/kotlin/com/sunmi/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/sunmi/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,4 +1,21 @@
 package com.sunmi.portfolio.presentation.interceptor
 
-class PresentationInterceptor {
+import com.sunmi.portfolio.domain.entity.HttpInterface
+import com.sunmi.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+
+@Component
+class PresentationInterceptor(  // 방문하는 모든 클라이언트의 정보를 저장
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    // 예외 발생과 상관없이 동작해야하므로 afterCompletion 사용
+    override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+
 }
--- src/main/kotlin/com/sunmi/portfolio/presentation/interceptor/PresentationInterceptorConfig.kt
+++ src/main/kotlin/com/sunmi/portfolio/presentation/interceptor/PresentationInterceptorConfig.kt
@@ -1,4 +1,18 @@
 package com.sunmi.portfolio.presentation.interceptor
 
-class PresentationInterceptorConfig {
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class PresentationInterceptorConfig(
+    private val presentationInterceptor: PresentationInterceptor
+) : WebMvcConfigurer {
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(presentationInterceptor)
+            .addPathPatterns("/**")  // 모든 경로
+            .excludePathPatterns("/assets/**", "/css/**", "/js/**", "/admin/**", "/h2**", "/favicon.ico", "/error")
+    }
+
 }
